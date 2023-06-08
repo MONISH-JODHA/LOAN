902642cbdd89f30c4b51fe3abb01cfa86fbdd7df
monishjodha <monish.21jics202@jietjodhpur.ac.in>
2023-06-08 16:19:15 +0530

    validation is added

--- show.js
+++ show.js
@@ -14,5 +14,12 @@ function myFunction(){
 }
 
 function validate(){
-    
+    const m=document.getElementById("m").value;
+    const m1=document.getElementById("m1").value;
+    const m2=document.getElementById("m2").value;
+    if(m1=="",m=="",m2==""){
+        alert("Please fill all the fields");
+    }
+
+
 }
